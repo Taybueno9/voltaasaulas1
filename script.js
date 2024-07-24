304673319485a8bf9431229b4812e004d15aafb1
Taybueno9 <130675653+Taybueno9@users.noreply.github.com>
2024-07-24 11:23:25 -0300

    atividade1

---
+++ script.js
@@ -0,0 +1,20 @@
+document.addEventListener(DOMContentLoard;()=>{
+    const button = document.queryselector('button');
+    button.addEventListener('click',()=>{
+        alert(Estamos animados para começar o ano com vocês!');
+        });
+
+const startDate=new Date('2024-07-24');
+cont today=newDate();
+const timeDiff=startDate-today;
+const daysLeft=Match.ceili(timeDiff/(1000*60*60*24));
+
+const countdownElement=document.createElement('p');
+countdownElement.textContent='Faltram ${daysLeft} dias para o inicio das aulas!';
+document.queryselector('.card').appendChild(countdownElement);
+
+constimage=documentqueryselector('img');
+image.addEventListener(click;()=>{
+    image,src=voltaaulas.jpg;
+});
+
